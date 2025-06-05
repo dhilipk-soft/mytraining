d06a90806cf980a0bf8e489fca3685a43be60ed2
Nitish Bharadwaj B <nitishbharadwaj.softsuave@gmail.com>
2025-06-05 19:33:35 +0530

    todo

---
+++ date13/src/components/ListShow/Flight.jsx
@@ -0,0 +1,8 @@
+
+export default function Flight({flight}){
+    return(
+        <div className="flight">
+            <h1>Flight</h1>
+        </div>
+    )
+}
---
+++ date13/src/components/ListShow/FlightList.jsx
@@ -0,0 +1,10 @@
+
+export default function FlightList({flights}) {
+    return (
+        <div className="flightList">
+            <div className="flightlistContainer">
+                
+            </div>
+        </div>
+    )
+}
